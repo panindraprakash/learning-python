abd71d83ac44ce1354943717bd9936efe2c3be95
panindraprakash <panindraprakash@gmail.com>
2016-08-14 02:46:32 +0530

    complete exercise 13

---
+++ learnpythonthehardway/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv 
+
+script, first, second, third = argv
+
+print "The script is called:", script # printing the running script value
+print "Your first variable is:", first # printing entred value during run time to the first variable
+print "Your second variable is:", second # printing entred value during run time  to the second variable
+print "Your third variable is:", third # printing entred value during run time  to the third variable
+# during running program the runing instrution is python ex13.py first 2nd 3rd or python ex13.py stuff things that, etx
