23657becaa60c79d52018ef7f53067cff30585ae
panindraprakash <panindraprakash@gmail.com>
2016-08-13 23:43:42 +0530

    complete exercise 10

---
+++ learnpythonthehardway/ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."  # Assigning the vales to tabby_cat and \t as Horizontal Tab.
+persian_cat = "I'm split\non a line." # Assigning the vales to persian_cat and \n for new line.
+backslash_cat = "I'm \\ a \\ cat." # Assigning the vales to backslash_cat and \\ for single \ .
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""# Assigning the vales to fat_cat and \t as Horizontal Tab.
+
+print tabby_cat # printing value of tabby_cat.	
+print persian_cat # printing value of  persian_cat.
+print backslash_cat # printing value of backslash_cat.
+print fat_cat # printing value of fat_cat.
