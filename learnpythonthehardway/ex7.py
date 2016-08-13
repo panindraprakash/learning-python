35136748dfddc7bacf01714688b6699a8b33a4e8
panindraprakash <panindraprakash@gmail.com>
2016-08-13 20:43:02 +0530

    complete exercise 7

---
+++ learnpythonthehardway/ex7.py
@@ -0,0 +1,24 @@
+print "Mary had a little lamb." # printing what mary had		
+print "Its fleece was white as %s." % 'snow' # printing who it looks like
+print "And everywhere that Mary went." #printing
+print "." * 10  # what'd that do? # . is multipling 10 times
+
+end1 = "C" # assigning value c to end1
+end2 = "h" # assigning value h to end2
+end3 = "e" # assigning value e to end3
+end4 = "e" # again we are assigning value e to end4 
+end5 = "s" # assigning value s to end5
+end6 = "e" # again we are assigning value e to end6
+end7 = "B" # assigning value b to end7
+end8 = "u" # assigning value u to end8
+end9 = "r" # assigning value r to end9
+end10 = "g" # assigning value g to end10
+end11 = "e" # again we assigning value e to end11
+end12 = "r" # assigning value r to end12
+
+print end1 + end2 + end3 + end4 + end5 + end6, # concating # using comma at the end # we can also use same assigined value as many times
+print end7 + end8 + end9 + end10 + end11 + end12
+
+print end1 + end2 + end3 + end4 + end5 + end6  # testing by removing comma # this printing line will printed in one line
+print end7 + end8 + end9 + end10 + end11 + end12  # this printing line will printed in another line
+
