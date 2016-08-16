03ef85c38d535efce31edd3de2181bb0de434471
panindraprakash <panindraprakash@gmail.com>
2016-08-16 12:18:15 +0530

    complete exercise 15

--- learnpythonthehardway/ex15.py
+++ learnpythonthehardway/ex15.py
@@ -13,3 +13,9 @@ file_again = raw_input("> ") # inputing the value
 txt_again = open(file_again) # Assigning the vales to text_again
 
 print txt_again.read() # '.read' function we are printing the content in the defined file
+
+# to run this program create a new text file 
+# in this program i created a text file name as ex15_sample.txt and the contents are 
+# line 1 This is stuff I typed into a file.
+# line 2 It is really cool stuff.
+# line 3 Lots and lots of fun to have in here.
